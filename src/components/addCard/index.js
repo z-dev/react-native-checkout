68321a3681b246b9f42a00b16121cf391bcd60dc
john <johnagill95@gmail.com>
2016-07-14 16:06:38 +0100

    added add card component and stripe add card component

---
+++ src/components/addCard/index.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react'
+import { View, StyleSheet, TextInput, TouchableHighlight, Text } from 'react-native'
+
+const styles = StyleSheet.create({
+  test: {
+    marginTop: 100
+  },
+  highlight: {
+    backgroundColor: 'grey',
+    height: 40,
+    flex: 1,
+    justifyContent: 'center'
+  },
+  textInput: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1
+  },
+  errorStyle: {
+
+  }
+})
+
+export class AddCard extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { cardNumber: '', expiryDate: '', cvc: '' }
+  }
+
+  render() {
+    return (
+      <View style={styles.test}>
+        <TextInput
+          style={styles.textInput}
+          onChangeText={(cardNumber) => this.setState({ cardNumber })}
+          value={this.state.cardNumber}
+          placeholder={'4242424242424242'}
+        />
+        <TextInput
+          style={styles.textInput}
+          onChangeText={(expiryDate) => this.setState({ expiryDate })}
+          value={this.state.expiryDate}
+          placeholder={'3/19'}
+        />
+        <TextInput
+          style={styles.textInput}
+          onChangeText={ (cvc) => this.setState({ cvc }) }
+          value={this.state.cvc}
+          placeholder={'123'}
+        />
+        <TouchableHighlight style={[styles.highlight, this.props.highlightStyle]} onPress={() => this.props.addCard(this.state.cardNumber, this.state.expiryDate, this.state.cvc)}>
+          <Text>
+            + Add Card
+          </Text>
+        </TouchableHighlight>
+      </View>
+    )
+  }
+}
---
+++ src/components/stripeAddCard/index.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import { getCardToken } from '../../common/stripe'
+
+import { AddCard } from '../addCard'
+
+export class StripeAddCard extends Component {
+
+  render() {
+    return (
+      <AddCard addCard={(cardNumber, expiryDate, cvc) => {
+        getCardToken(cardNumber, expiryDate, cvc, this.props.publicStripeKey)
+          .then((token) => this.props.response(token))
+      }}
+      />
+    )
+  }
+}
