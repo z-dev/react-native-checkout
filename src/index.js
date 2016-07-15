db40dfc7a85a34cbb9ab3cb1e2530657947d994a
john <johnagill95@gmail.com>
2016-07-15 11:53:22 +0100

    added index file for importing components

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import StripeAddCard1 from './components/stripeAddCard'
+import AddCard1 from './components/addCard'
+import SelectPayment1 from './components/selectPayment'
+
+export const StripeAddCard = StripeAddCard1
+export const AddCard = AddCard1
+export const SelectPayment = SelectPayment1
